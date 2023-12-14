56bb129788e76178fbf32b26eb79772dc2c902aa
IamTyrone <tyronemguni@gmail.com>
2023-12-14 16:34:34 +0200

    Posts api documentation

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ export const options = {
       version: "1.0.0",
     },
   },
-  apis: ["src/users/routes/*.ts"],
+  apis: ["src/users/routes/*.ts", "src/posts/routes/*.ts"],
 };
 
 const openapiSpecification = swaggerJSDoc(options);
--- src/posts/routes/index.ts
+++ src/posts/routes/index.ts
@@ -1,3 +1,196 @@
+/**
+ * @swagger
+ * /api/v1/posts:
+ *   post:
+ *     title:: Create Post
+ *     summary: Create a new post
+ *     requestBody:
+ *         required: true
+ *         description: All fields are required on this endpoint.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                       title:
+ *                         type: string
+ *                         description: Title of the blog post
+ *                         example: Why PHP is not a real language.
+ *                       content:
+ *                         type: string
+ *                         description: The actual text content of the blog post
+ *                         example: PHP is the red headed stepchild of the programming languages world because.....
+ *                       published:
+ *                          type: boolean
+ *                          description: Shows whether or not the blog post has been published
+ *                          example: false
+ *                       author:
+ *                          type: integer
+ *                          description: ID of the author of the blog post
+ *                          example: 1
+ *     responses:
+ *       201:
+ *         description: Creates a user and returns the user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The user ID.
+ *                         example: 3
+ *                       title:
+ *                         type: string
+ *                         description: Title of the blog post
+ *                         example: Why PHP is not a real language.
+ *                       content:
+ *                         type: string
+ *                         description: The actual text content of the blog post
+ *                         example: PHP is the red headed stepchild of the programming languages world because.....
+ *                       published:
+ *                          type: boolean
+ *                          description: Shows whether or not the blog post has been published
+ *                          example: false
+ *                       author:
+ *                          type: integer
+ *                          description: ID of the author of the blog post
+ *                          example: 1
+ */
+
+/**
+ * @swagger
+ * /api/v1/posts:
+ *   get:
+ *     title:: Get Posts
+ *     summary: Retrive all posts in the system.
+ *     responses:
+ *       200:
+ *         description: Returns a list of post json objects.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The post ID.
+ *                         example: 3
+ *                       title:
+ *                         type: string
+ *                         description: Title of the blog post
+ *                         example: Why PHP is not a real language.
+ *                       content:
+ *                         type: string
+ *                         description: The actual text content of the blog post
+ *                         example: PHP is the red headed stepchild of the programming languages world because.....
+ *                       published:
+ *                          type: boolean
+ *                          description: Shows whether or not the blog post has been published
+ *                          example: false
+ *                       author:
+ *                          type: integer
+ *                          description: ID of the author of the blog post
+ *                          example: 1
+ */
+
+/**
+ * @swagger
+ * /api/v1/posts/:id:
+ *   get:
+ *     title:: Get Single POST
+ *     summary: Retrive a single post from system.
+ *     responses:
+ *       200:
+ *         description: Returns a single post from the system.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The post ID.
+ *                         example: 3
+ *                       title:
+ *                         type: string
+ *                         description: Title of the blog post
+ *                         example: Why PHP is not a real language.
+ *                       content:
+ *                         type: string
+ *                         description: The actual text content of the blog post
+ *                         example: PHP is the red headed stepchild of the programming languages world because.....
+ *                       published:
+ *                          type: boolean
+ *                          description: Shows whether or not the blog post has been published
+ *                          example: false
+ *                       author:
+ *                          type: integer
+ *                          description: ID of the author of the blog post
+ *                          example: 1
+ */
+
+/**
+ * @swagger
+ * /api/v1/posts/:id:
+ *   patch:
+ *     title:: Update post
+ *     summary: Update a post
+ *     requestBody:
+ *         required: true
+ *         description: Updates a post and returns the post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                       title:
+ *                         type: string
+ *                         description: Title of the blog post
+ *                         example: Why PHP is not a real language.
+ *                       content:
+ *                         type: string
+ *                         description: The actual text content of the blog post
+ *                         example: PHP is the red headed stepchild of the programming languages world because.....
+ *                       published:
+ *                          type: boolean
+ *                          description: Shows whether or not the blog post has been published
+ *                          example: false
+ *                       author:
+ *                          type: integer
+ *                          description: ID of the author of the blog post
+ *                          example: 1
+ *     responses:
+ *       200:
+ *         description: Updates a posts and returns the posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The post ID.
+ *                         example: 3
+ *                       title:
+ *                         type: string
+ *                         description: Title of the blog post
+ *                         example: Why PHP is not a real language.
+ *                       content:
+ *                         type: string
+ *                         description: The actual text content of the blog post
+ *                         example: PHP is the red headed stepchild of the programming languages world because.....
+ *                       published:
+ *                          type: boolean
+ *                          description: Shows whether or not the blog post has been published
+ *                          example: false
+ *                       author:
+ *                          type: integer
+ *                          description: ID of the author of the blog post
+ *                          example: 1
+ */
+
 import { Router } from "express";
 import CreatePost from "../controllers/createPost";
 import authenticate from "../../middleware/authenticate";
